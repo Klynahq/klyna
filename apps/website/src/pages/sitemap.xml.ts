ba2d0d841e0007d7c9f88771ef6953131655426b
adeedaxguy <70172982+adeedaxguy@users.noreply.github.com>
2026-09-02 01:43:40 +0500

    Fix Klyna indexability and Shopify install paths

---
+++ apps/website/src/pages/sitemap.xml.ts
@@ -0,0 +1,18 @@
+export const prerender = true;
+
+export async function GET() {
+  const lastmod = new Date().toISOString();
+  const body = `<?xml version="1.0" encoding="UTF-8"?>
+<sitemapindex xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+  <sitemap>
+    <loc>https://klyna.dev/sitemap-0.xml</loc>
+    <lastmod>${lastmod}</lastmod>
+  </sitemap>
+</sitemapindex>`;
+
+  return new Response(body, {
+    headers: {
+      "Content-Type": "application/xml; charset=utf-8",
+    },
+  });
+}
